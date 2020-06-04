34b75bc843a37b0cb3df5adf8cab7ef01abb5e8d
bcabrera19 <bryan.cabrera19@gmail.com>
2020-06-04 17:10:00 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <nav class="navbar nav-background">
+      <a class="navbar-brand" href="#">
+        <img src="assets/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        Fit Food Tracker
+      </a>
+
+    <button class="btn btn-primary" type="submit">+</button>
+    </nav>
+
+    <div class="container list">
+      <div class="row align-items-start text-center">
+        <div class="col-5">
+          <h5>Description</h5>
+        </div>
+        <div class="col-sm">
+          <h5>Calories</h5>
+        </div>
+        <div class="col-sm">
+          <h5>Carbs</h5>
+        </div>
+        <div class="col-sm">
+          <h5>Protein</h5>
+        </div>
+        <div class="col-sm">
+          <h5>Fat</h5>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Lato:400,700');
+
+  body {
+    background-image: #EEF1F4;
+  }
+
+  .nav-background {
+    background: rgb(13, 82, 17);
+  }
+
+  .navbar-brand {
+    color: #EEF1F4;
+  }
+
+  .navbar-brand:hover {
+    color: #EEF1F4;
+  }
+
+  .list {
+    background: #EEF1F4;
+  }
+
+
+</style>
---
+++ src/pages/Registration/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <b-container>
+      <b-row align-h="center" class="mt-5">
+        <b-col cols="5">
+          <b-card class="p-3">
+            <h3 class="mb-4 text-center">Fit Food Tracker</h3>
+            <form>
+              <div class="form-group">
+                <input type="email" class="form-control" v-model="form.email" required placeholder="Phone number, username or email">
+              </div>
+              <div class="form-group">
+                <input type="password" class="form-control" v-model="form.password" required placeholder="Password">
+              </div>
+              <button type="submit" class="btn btn-primary">Log In</button>
+            </form>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Lato:400,700');
+
+  body {
+    background: #EEF1F4;
+    font-family: 'Lato', sans-serif;
+  }
+
+</style>
